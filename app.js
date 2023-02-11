8beed1ce4927e12d57c64e0e3bf128ce6e4a5668
Alex <84993822+a-zed@users.noreply.github.com>
2023-02-11 14:30:44 +0700

    Complete 'game' function

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
 
 // *** Game Function ***
-// Get number of rounds user wants to play
-// While there is no winner
-    // Play round
-    // Update score
-// Print winner of game
+function game(){
 
+    // Get number of rounds user wants to play
+    let rounds = parseInt(prompt("How many rounds do you want to play?", "5"));
+
+    // Calculate number of round wins required to win the game (majority)
+    let wins_required = Math.floor((rounds / 2) + 1);
+
+    // declare variables to track scores and winner of each round
+    let user_score = 0;
+    let computer_score = 0;
+    let round_winner;
+
+    // While there is no winner
+    while (user_score < wins_required && computer_score < wins_required){
+
+        // Play round and store winner
+        round_winner = round();
+
+        // Update scores (no score increase if round is a draw)
+        if (round_winner === "computer"){
+            computer_score++;
+        }
+
+        else if (round_winner === "user"){
+            user_score++;
+        }
+    }
+    // Print winner of game
+    if (user_score > computer_score){
+        console.log("You win the game!")
+    }
+
+    else {
+        console.log("The computer wins the game! Better luck next time.")
+    }
+}
 
 
 // *** Round Function ***
