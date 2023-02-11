d253b4709f73ee8abce233c32f6389f4ef38423c
Alex <84993822+a-zed@users.noreply.github.com>
2023-02-11 12:01:12 +0700

    Add pseudocode to app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+
+// *** Game Function ***
+// Get number of rounds user wants to play
+// While there is no winner
+    // Play round
+    // Update score
+// Print winner of game
+
+
+
+// *** Round Function ***
+// Generate random action for the computer
+// Get action input from user (rock, paper or scissors)
+// Determine winner
+// Notify user of the result
+// Return winner
