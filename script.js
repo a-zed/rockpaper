483847f4bb4afa1175d0bd71a6758d724581f326
Alex <84993822+a-zed@users.noreply.github.com>
2023-03-24 12:48:45 +0800

    style: Rename javascript file to script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// Play game
+function playGame(){
+
+  // Get the score user wants to play to
+  let winningScore = prompt("What score do you want to play to? ");
+
+  // Set computer + human scores to zero
+  let computerScore = 0;
+  let humanScore = 0;
+
+  // While both scores are less than score specified
+  while(computerScore < winningScore &&
+        humanScore < winningScore){
+
+          // Play Round and store winner
+          let roundWinner = playRound();
+
+          // Update scores
+
+
+        }
+
+    
+
+  // print winner
+}
