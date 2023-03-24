c1f96228bfbf17a7b53240fe11752ee2b96f68f6
Alex <84993822+a-zed@users.noreply.github.com>
2023-03-24 12:10:51 +0800

    docs: Write pseudocode

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// 
+// Play game
+
+  // Get the score user wants to play to
+
+  // Set computer + human scores to zero
+
+  // While both scores are less than score specified
+
+    // Play Round
+
+    // Update scores
+
+  // print winner
