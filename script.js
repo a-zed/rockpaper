5d17b5a9e92e0231127ea343b238bb6f58fa0759
Alex <84993822+a-zed@users.noreply.github.com>
2023-03-30 13:41:40 +0800

    feature: Wrap playGame function in IIFE, add UI variables and game environment function

--- script.js
+++ script.js
@@ -1,167 +1,204 @@
-// Play a game of rock, paper, scissors
-function playGame(){
+// Play a game of rock, paper, scissors (IIFE)
+(function(){
 
-  let winningScore = getScoreRequiredToWin();
+  // assign UI elements to variables
+  let introSection = document.querySelector(".intro");
+  let playSection = document.querySelector(".play");
+  let winRoundSection = document.querySelector(".win-round");
+  let loseGameSection = document.querySelector(".lose-game");
+  let winGameSection = document.querySelector(".win-game");
 
-  let computerScore = 0;
-  let humanScore = 0;
 
-  while(computerScore < winningScore &&
-        humanScore < winningScore){
 
-          let roundWinner = playRoundAndGetWinner();
 
-          if (roundWinner === "human"){
 
-            humanScore++;
+  let startButton = document.querySelector(".glow-button");
 
-          }
+  startButton.addEventListener("click", playGame);
 
-          if (roundWinner === "computer"){
 
-            computerScore++;
+  function playGame(){
 
-          }
+    createPlayingEnvironment();
 
-          printCurrentScore(computerScore, humanScore);
-
-        }
-
-  printGameWinner(computerScore, humanScore);
-}
-
-// Get the score required to win from user (validated) and returns it
-function getScoreRequiredToWin(){
-
-  let winningScore = parseFloat(prompt("What score do you want to play to? "));
-
-  while (winningScore <= 0 ||
-         Number.isInteger(winningScore) === false){
-
-          winningScore = parseFloat(prompt("What score do you want to play to? (it has to be a positive whole number) "))
-
-         }
+    let winningScore = getScoreRequiredToWin();
   
-  return winningScore;
-
-}
-
-// Play a round of rock, paper, scissors. It then prints the round winner and returns it
-function playRoundAndGetWinner(){
-
-  let computerAction = generateRandomAction();
-
-  let humanAction = getHumanAction();
-
-  let roundWinner = determineRoundWinner(computerAction, humanAction);
-
-  printRoundWinner(roundWinner, computerAction, humanAction);
-
-  return roundWinner;
-
-}
-
-// Generate a random "rock, paper or scissors" action for the computer
-function generateRandomAction(){
-
-  // generate random number from 1 to 3
-  let randomNumber = Math.floor(Math.random() * 3) + 1;
-
-  if (randomNumber === 1){
-    return "rock";
+    let computerScore = 0;
+    let humanScore = 0;
+  
+    while(computerScore < winningScore &&
+          humanScore < winningScore){
+  
+            let roundWinner = playRoundAndGetWinner();
+  
+            if (roundWinner === "human"){
+  
+              humanScore++;
+  
+            }
+  
+            if (roundWinner === "computer"){
+  
+              computerScore++;
+  
+            }
+  
+            printCurrentScore(computerScore, humanScore);
+  
+          }
+  
+    printGameWinner(computerScore, humanScore);
   }
 
-  if (randomNumber === 2){
-    return "paper";
+  // Set up the UI for the rock, paper, scissors game
+  function createPlayingEnvironment(){
+
+    introSection.style.display = "none";
+    playSection.style.display = "flex";
+    winRoundSection.style.display = "none";
+    loseGameSection.style.display = "none";
+    winGameSection.style.display = "none";
   }
-
-  else {
-    return "scissors";
+  
+  // Get the score required to win from user (validated) and returns it
+  function getScoreRequiredToWin(){
+  
+    let winningScore = parseFloat(prompt("What score do you want to play to? "));
+  
+    while (winningScore <= 0 ||
+           Number.isInteger(winningScore) === false){
+  
+            winningScore = parseFloat(prompt("What score do you want to play to? (it has to be a positive whole number) "))
+  
+           }
+    
+    return winningScore;
+  
   }
-}
-
-// Get rock, paper or scissor action from user (validated)
-function getHumanAction(){
-
-  let humanAction = prompt("Enter your choice for this round - rock, paper or scissors ").toLowerCase();
-
-  while (humanAction !== "rock" &&
-         humanAction !== "paper" &&
-         humanAction !== "scissors"){
-
-     humanAction = prompt("Enter your choice for this round - rock, paper or scissors (making sure to spell it correctly) ");
-
+  
+  // Play a round of rock, paper, scissors. It then prints the round winner and returns it
+  function playRoundAndGetWinner(){
+  
+    let computerAction = generateRandomAction();
+  
+    let humanAction = getHumanAction();
+  
+    let roundWinner = determineRoundWinner(computerAction, humanAction);
+  
+    printRoundWinner(roundWinner, computerAction, humanAction);
+  
+    return roundWinner;
+  
+  }
+  
+  // Generate a random "rock, paper or scissors" action for the computer
+  function generateRandomAction(){
+  
+    // generate random number from 1 to 3
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+  
+    if (randomNumber === 1){
+      return "rock";
     }
-
-return humanAction;
-
-}
-
-// Determine the round winner and return it
-function determineRoundWinner(computerAction, humanAction){
-
-  if (computerAction === humanAction){
-
-    return "draw";
-
-  }
-
-  if ((computerAction === "rock" && humanAction === "scissors") ||
-      (computerAction === "paper" && humanAction === "rock") ||
-      (computerAction === "scissors" && humanAction === "paper")){
-
-        return "computer";
-      }
-
-  return "human";
-
-}
-
-// Print out the round winner
-function printRoundWinner(roundWinner, computerAction, humanAction){
-
-  if (roundWinner === "draw"){
-
-    console.log(`You both played ${computerAction}, so this round is a draw!`);
-    
-  }
-
-  else if (roundWinner === "computer"){
-
-    console.log(`${computerAction} beats ${humanAction}, so the computer wins this round!`);
-    
-  }  
-
-  else {
-
-    console.log(`${humanAction} beats ${computerAction}, so you win this round!`);
-
+  
+    if (randomNumber === 2){
+      return "paper";
+    }
+  
+    else {
+      return "scissors";
+    }
   }
   
-}
-
-// Print out the current score
-function printCurrentScore(computerScore, humanScore){
-
-  console.log(`Human: ${humanScore}   Computer: ${computerScore}`);
-
-}
-
-// Print out the winner of the game
-function printGameWinner(computerScore, humanScore){
-
-  if (computerScore > humanScore){
-
-    console.log(`You lost the game! The final score was --> Human: ${humanScore}   Computer: ${computerScore}`);
-
+  // Get rock, paper or scissor action from user (validated)
+  function getHumanAction(){
+  
+    let humanAction = prompt("Enter your choice for this round - rock, paper or scissors ").toLowerCase();
+  
+    while (humanAction !== "rock" &&
+           humanAction !== "paper" &&
+           humanAction !== "scissors"){
+  
+       humanAction = prompt("Enter your choice for this round - rock, paper or scissors (making sure to spell it correctly) ");
+  
+      }
+  
+  return humanAction;
+  
+  }
+  
+  // Determine the round winner and return it
+  function determineRoundWinner(computerAction, humanAction){
+  
+    if (computerAction === humanAction){
+  
+      return "draw";
+  
+    }
+  
+    if ((computerAction === "rock" && humanAction === "scissors") ||
+        (computerAction === "paper" && humanAction === "rock") ||
+        (computerAction === "scissors" && humanAction === "paper")){
+  
+          return "computer";
+        }
+  
+    return "human";
+  
+  }
+  
+  // Print out the round winner
+  function printRoundWinner(roundWinner, computerAction, humanAction){
+  
+    if (roundWinner === "draw"){
+  
+      console.log(`You both played ${computerAction}, so this round is a draw!`);
+      
+    }
+  
+    else if (roundWinner === "computer"){
+  
+      console.log(`${computerAction} beats ${humanAction}, so the computer wins this round!`);
+      
+    }  
+  
+    else {
+  
+      console.log(`${humanAction} beats ${computerAction}, so you win this round!`);
+  
+    }
+    
+  }
+  
+  // Print out the current score
+  function printCurrentScore(computerScore, humanScore){
+  
+    console.log(`Human: ${humanScore}   Computer: ${computerScore}`);
+  
+  }
+  
+  // Print out the winner of the game
+  function printGameWinner(computerScore, humanScore){
+  
+    if (computerScore > humanScore){
+  
+      console.log(`You lost the game! The final score was --> Human: ${humanScore}   Computer: ${computerScore}`);
+  
+    }
+  
+    else {
+  
+      console.log(`You won the game! The final score was Human: ${humanScore}   Computer: ${computerScore}`);
+  
+    }
   }
 
-  else {
 
-    console.log(`You won the game! The final score was Human: ${humanScore}   Computer: ${computerScore}`);
 
-  }
-}
+
+});
+
+
 
 
 
